ec4149fbe15118c3b8c26555df29241abc3d7ba3
Keshav Lakshmi Narasimhan <keshavlakshminarasimhan@gmail.com>
2025-11-09 21:01:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -30,7 +29,6 @@ setup(
         'console_scripts': [
             'keyboardcontrol = movepanda.keyboardcontrol:main',
             'getpositions = movepanda.getpositions:main',
-            'handtracker = movepanda.handtracker:main',
             'manipulate = movepanda.manipulate:main'
         ],
     },
